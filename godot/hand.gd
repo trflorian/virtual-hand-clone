cf763c293a1b60d9e32574aa536cff812336228e
trflorian <florian.trautweiler@hispeed.ch>
2024-06-30 19:35:15 +0200

    cleanup godot code by extracting hand code to separate script than udp communication

---
+++ godot/hand.gd
@@ -0,0 +1,57 @@
+extends Node3D
+
+class_name Hand
+
+const NUM_LANDMARKS: int = 21
+const HAND_SCALE: float = 30.0
+
+const HAND_LINES_MAPPING = [
+	[0, 1], [1, 2], [2, 3], [3, 4], # Thumb
+	[0, 5], [5, 6], [6, 7], [7, 8], # Index Finger
+	[5, 9], [9, 10], [10, 11], [11, 12], # Middle Finger
+	[9, 13], [13, 14], [14, 15], [15, 16], # Ring Finger
+	[0, 17], [13, 17], [17, 18], [18, 19], [19, 20], # Pinky
+]
+
+var landmark_sphere: PackedScene = preload("res://landmark_sphere.tscn")
+
+var hand_landmarks: Array[Node3D] = []
+var hand_lines: Array[MeshInstance3D] = []
+
+func _ready() -> void:
+	_create_hand_landmark_spheres()
+	_create_hand_lines()
+
+func _process(_delta: float) -> void:
+	_update_hand_lines()
+
+func _create_hand_landmark_spheres() -> void:
+	for i in range(NUM_LANDMARKS):
+		var landmark_instance = landmark_sphere.instantiate()
+		add_child(landmark_instance)
+		hand_landmarks.append(landmark_instance)
+
+func _create_hand_lines() -> void:
+	for i in HAND_LINES_MAPPING.size():
+		var line_instance := MeshInstance3D.new()
+		add_child(line_instance)
+		hand_lines.append(line_instance)
+
+func _update_hand_lines() -> void:
+	for i in HAND_LINES_MAPPING.size():
+		var mapping = HAND_LINES_MAPPING[i]
+		var p0 = hand_landmarks[mapping[0]].global_position
+		var p1 = hand_landmarks[mapping[1]].global_position
+		Draw3D.edit_line(hand_lines[i], p0, p1)
+
+func update_hand_landmark(landmark_id: int, landmark_pos: Vector3) -> void:
+	var lm = hand_landmarks[landmark_id]
+	lm.global_position = lerp(lm.global_position, landmark_pos, 0.5)
+
+func parse_hand_landmarks_from_data(hand_data: Array) -> void:
+	for lm_id in range(NUM_LANDMARKS):
+		var lm_data = hand_data[lm_id]
+		var pos_cam = Vector3(lm_data[0], lm_data[1], lm_data[2]) - Vector3.ONE * 0.5
+		var pos_xyz = Vector3(-pos_cam[0], -pos_cam[1], pos_cam[2]) * HAND_SCALE
+		#pos_xyz.z /= scale
+		update_hand_landmark(lm_id, pos_xyz)
--- godot/virtual_hand_clone.gd
+++ godot/virtual_hand_clone.gd
@@ -1,110 +1,40 @@
 extends Node3D
 
-const NUM_LANDMARKS: int = 21
-const HAND_SCALE: float = 30.0
 const PORT: int = 4242
 
-@export var landmark_sphere: PackedScene
-@export var hand_skeleton: Skeleton3D
-
-var landmarks: Array[Node3D] = []
-var hand_lines: Array[MeshInstance3D] = []
-
-const hand_lines_mapping = [
-	# Thumb
-	[0, 1],
-	[1, 2],
-	[2, 3],
-	[3, 4],
-	# Index Finger
-	[0, 5],
-	[5, 6],
-	[6, 7],
-	[7, 8],
-	# Middle Finger
-	[5, 9],
-	[9, 10],
-	[10, 11],
-	[11, 12],
-	# Ring Finger
-	[9, 13],
-	[13, 14],
-	[14, 15],
-	[15, 16],
-	# Pinky
-	[0, 17],
-	[13, 17],
-	[17, 18],
-	[18, 19],
-	[19, 20],
-]
-
 var server: UDPServer
+
+var hand := Hand.new()
 
 func _ready() -> void:
 	server = UDPServer.new()
 	server.listen(PORT)
 	
-	for i in range(NUM_LANDMARKS):
-		var landmark_instance = landmark_sphere.instantiate()
-		add_child(landmark_instance)
-		
-		landmarks.append(landmark_instance)
-	
-	for i in hand_lines_mapping.size():
-		var instance := MeshInstance3D.new()
-		add_child(instance)
-		hand_lines.append(instance)
+	add_child(hand)
 
-func _parse_hands_from_packet(data: PackedByteArray):
+func _parse_hands_from_packet(data: PackedByteArray) -> Array:
 	var json_string = data.get_string_from_utf8()
 	var json = JSON.new()
 	var error = json.parse(json_string)
 	if error == OK:
 		var data_received = json.data
 		if typeof(data_received) == TYPE_ARRAY:
-			if len(data_received) == 0:
-				return null
-				
-			# extract first hand
-			var hand = data_received[0]
-			return hand
+			return data_received
 		else:
 			print("Unexpected data")
-			return null
+			return []
 	else:
 		print("JSON Parse Error: %s in %s at line %d" % [json.get_error_message(), json_string, json.get_error_line()])
-		return null
-	
+		return []
 
 func _process(delta: float) -> void:
 	server.poll()
 	if server.is_connection_available():
 		var peer = server.take_connection()
 		var data = peer.get_packet()
-		var hand = _parse_hands_from_packet(data)
+		var hands_data = _parse_hands_from_packet(data)
 		
-		if hand != null:
-			var pos0 = Vector3(hand[0][0], hand[0][1], hand[0][2])
-			var pos1 = Vector3(hand[1][0], hand[1][1], hand[1][2])
+		if len(hands_data) > 0:
+			var hand_data = hands_data[0]
 			
-			var scale = (pos1 - pos0).length()
-			#scale = max(min(scale, 0.01), 10.0)
-			for lm_id in range(NUM_LANDMARKS):
-				var hand_coord = hand[lm_id]
-				var pos_cam = Vector3(hand_coord[0], hand_coord[1], hand_coord[2]) - Vector3.ONE * 0.5
-				var pos_xyz = Vector3(-pos_cam[0], -pos_cam[1], pos_cam[2])
-				#pos_xyz.z /= scale
-				landmarks[lm_id].global_position = lerp(landmarks[lm_id].global_position, pos_xyz * HAND_SCALE, 0.5)
-			
-			for i in hand_lines_mapping.size():
-				var mapping = hand_lines_mapping[i]
-				var p0 = landmarks[mapping[0]].global_position
-				var p1 = landmarks[mapping[1]].global_position
-				
-				Draw3D.edit_line(hand_lines[i], p0, p1)
-				
-				#print(lm_id, " ", hand_skeleton.get_bone_name(lm_id))
-				#var parent_bone_id = hand_skeleton.get_bone_parent(lm_id)
-				#if parent_bone_id == -1:
-					#hand_skeleton.set_bone_pose_position(lm_id, pos_xyz )
+			hand.parse_hand_landmarks_from_data(hand_data)
