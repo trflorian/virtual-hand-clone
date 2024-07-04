049d5a63e718f3fe68bfe08e3dd7694258632ec8
trflorian <florian.trautweiler@hispeed.ch>
2024-07-04 20:39:25 +0200

    track left and right hand separately

--- godot/hand_tracking.gd
+++ godot/hand_tracking.gd
@@ -4,31 +4,32 @@ const PORT: int = 4242
 
 var server: UDPServer
 
-var hands: Array = []
+var left_hand: Hand
+var right_hand: Hand
 
 func _ready() -> void:
 	server = UDPServer.new()
 	server.listen(PORT)
+	
+	left_hand = _create_new_hand()
+	right_hand = _create_new_hand()
 
-func _parse_hands_from_packet(data: PackedByteArray) -> Array:
+func _parse_hands_from_packet(data: PackedByteArray) -> Dictionary:
 	var json_string = data.get_string_from_utf8()
 	var json = JSON.new()
+	
 	var error = json.parse(json_string)
-	if error == OK:
-		var data_received = json.data
-		if typeof(data_received) == TYPE_ARRAY:
-			return data_received
-		else:
-			print("Unexpected data")
-			return []
-	else:
-		print("JSON Parse Error: %s in %s at line %d" % [json.get_error_message(), json_string, json.get_error_line()])
-		return []
+	assert(error == OK)
+	
+	var data_received = json.data
+	assert(typeof(data_received) == TYPE_DICTIONARY)
+	
+	return data_received
 
-func _create_new_hand() -> void:
+func _create_new_hand() -> Hand:
 	var hand_instance := Hand.new()
 	add_child(hand_instance)
-	hands.append(hand_instance)
+	return hand_instance
 
 func _process(_delta: float) -> void:
 	server.poll()
@@ -37,17 +38,8 @@ func _process(_delta: float) -> void:
 		var data = peer.get_packet()
 		var hands_data = _parse_hands_from_packet(data)
 		
-		var len_diff = len(hands_data) - len(hands)
-		
-		if len_diff > 0:
-			for i in range(len_diff):
-				_create_new_hand()
-		
-		if len_diff < 0:
-			for i in range(-len_diff):
-				# TODO: add hand identifier and track same hand
-				var last_hand: Hand = hands.pop_back()
-				last_hand.queue_free()
-		
-		for hand_id in hands_data.size():
-			hands[hand_id].parse_hand_landmarks_from_data(hands_data[hand_id])
+		if hands_data["left"] != null:
+			left_hand.parse_hand_landmarks_from_data(hands_data["left"])
+			
+		if hands_data["right"] != null:
+			right_hand.parse_hand_landmarks_from_data(hands_data["right"])
--- python/hand_detection.py
+++ python/hand_detection.py
@@ -8,6 +8,18 @@ import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawing
 import mediapipe.python.solutions.drawing_styles as mp_drawing_styles
 
+def extract_hand_type_index(multi_handedness, hand_type: str) -> int:
+    hand_classification = [
+        hand.classification[0] for hand in multi_handedness
+    ]
+
+    hands = filter(lambda x: x.label.lower() == hand_type and x.score > 0.0, hand_classification)
+    hands = sorted(hands, key=lambda x: x.score, reverse=True)
+
+    if len(hands) == 0:
+        return -1
+
+    return hand_classification.index(hands[0])
 
 def run_hand_tracking_server(
     server_ip: str,
@@ -35,24 +47,33 @@ def run_hand_tracking_server(
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = hands.process(frame_rgb)
 
-            hand_coords = []
+            hand_coords = {
+                "left": None,
+                "right": None,
+            }
 
-            # Draw the hand annotations on the image
             if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
+                for hand_type in ["left", "right"]:
+                    hand_idx = extract_hand_type_index(results.multi_handedness, hand_type)
+
+                    if hand_idx == -1:
+                        continue
+
+                    hand_lms = results.multi_hand_landmarks[hand_idx]
+
+                    hand_coords[hand_type] = [
+                        [landmark.x, landmark.y, landmark.z]
+                        for landmark in hand_lms.landmark
+                    ]
+
                     mp_drawing.draw_landmarks(
                         image=frame,
-                        landmark_list=hand_landmarks,
+                        landmark_list=hand_lms,
                         connections=mp_hands.HAND_CONNECTIONS,
                         landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
                         connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style(),
                     )
 
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    hand_coord_array = [[landmark.x, landmark.y, landmark.z] for landmark in hand_landmarks.landmark]
-                    hand_coords.append(hand_coord_array)
-            
             # Send the hand coordinates to the client
             encoded_coords = json.dumps(hand_coords)
             client_socket.sendto(encoded_coords.encode(), (server_ip, server_port))
